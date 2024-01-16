2ff23b5ffc8b8bac2c7f73c17338ea477c049538
solidityteam <126508206+solidityteam@users.noreply.github.com>
2024-01-16 11:14:29 -0500

    Update confirmation.tsx

--- apps/web/src/components/checkout-page/confirmation.tsx
+++ apps/web/src/components/checkout-page/confirmation.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useCourseContext } from "~/contexts/CourseContext";
-import { useUserContext } from "~/contexts/UserContext";
 import Link from "next/link";
 import { FilledButton } from "../buttons/filled-button";
 import { OutlineButton } from "../buttons/outline-button";
@@ -12,7 +11,6 @@ import { X } from "../icons/share/x";
 import { InviteFriends } from "../tee-time-page/invite-friends";
 
 export const Confirmation = ({ teeTimeId }: { teeTimeId: string }) => {
-  const { user } = useUserContext();
   const { course } = useCourseContext();
   return (
     <section className="mx-auto flex w-full flex-col gap-4 bg-white px-3 py-2 text-center md:max-w-[780px] md:rounded-xl md:p-6 md:py-4">
@@ -38,10 +36,10 @@ export const Confirmation = ({ teeTimeId }: { teeTimeId: string }) => {
       </p>
       <div className="flex w-full flex-col items-center justify-center gap-2 md:flex-row">
         <Link
-          href={`/${course?.id}/profile/${user?.id}`}
+          href={`/${course?.id}/my-tee-box`}
           className="w-full md:w-fit md:min-w-[250px]"
         >
-          <FilledButton className="w-full">Go to Profile</FilledButton>
+          <FilledButton className="w-full">Go To My Tee Box</FilledButton>
         </Link>
       </div>
       <InviteFriends isConfirmationPage={true} teeTimeId={teeTimeId} />
