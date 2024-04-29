55d62eeef6f4462662b3782dcda33cf7f0c657b0
Manish <manish.pandey2697@gmail.com>
2024-04-29 21:14:17 +0530

    Added flyout changes

--- apps/web/src/components/account-settings-page/payment-info.tsx
+++ apps/web/src/components/account-settings-page/payment-info.tsx
@@ -9,6 +9,9 @@ import { Trashcan } from "../icons/trashcan";
 import { Spinner } from "../loading/spinner";
 // import { AddCard } from "./add-card";
 import SidePanel from "./SidePanel";
+import { Close } from "../icons/close";
+import { FilledButton } from "../buttons/filled-button";
+import { OutlineButton } from "../buttons/outline-button";
 
 export const PaymentInfoMangeProfile = () => {
   const { cards, refetch, isLoading } = usePaymentMethods();
@@ -84,29 +87,66 @@ const CardDisplay = ({
           <Trashcan fill="#EE2020" className="w-[20px] h-[20px]" />
         </button>
       </div>
-      {confirmStatus ? (
-        <SidePanel isOpen={true}>
-          <div className="bg-white p-8 text-sm rounded shadow-md h-full">
-            <h2 className="text-lg font-semibold mb-4">Confirm Deletion</h2>
-            <p className="mb-2">Are you sure you want to delete card?</p>
-            <div className="flex justify-end">
-              <button
-                style={{ background: "red" }}
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded mr-2"
-                onClick={() => removeCard()}
-              >
-                Yes
-              </button>
-              <button
-                className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded"
-                onClick={() => setConfirmStatus(false)}
-              >
-                Cancel
-              </button>
+
+      <>
+      {confirmStatus && (
+        <div
+          className={`fixed left-0 top-0 z-20 h-[100dvh] w-screen backdrop-blur `}
+        >
+          <div className="h-screen bg-[#00000099]" />
+        </div>
+      )}
+      <aside
+        // ref={sidebar}
+        className={`!duration-400 fixed right-0 top-1/2 z-20 flex h-[90dvh] w-[80vw] -translate-y-1/2 flex-col overflow-y-hidden border border-stroke bg-white shadow-lg transition-all ease-linear sm:w-[500px] md:h-[100dvh] ${
+          confirmStatus ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="relative flex h-full flex-col">
+          <div className="flex items-center justify-between p-4">
+            <div className="text-lg">Remove saved card</div>
+
+            <button
+              // ref={trigger}
+              onClick={() => setConfirmStatus(false)}
+              aria-controls="sidebar"
+              aria-expanded={confirmStatus}
+              className="z-[2]"
+              aria-label="sidebarToggle"
+            >
+              <Close className="h-[25px] w-[25px]" />
+            </button>
+          </div>
+          <div className="flex h-full flex-col justify-between overflow-y-auto">
+            <div className="flex flex-col gap-6 px-0 sm:px-4">
+              <div>
+                <div className="mt-6 px-4 pb-4 text-center text-2xl font-[300] md:text-2xl">
+                  Are you sure you want to remove the card?
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 px-4 pb-6">
+              <div className="flex flex-col gap-2">
+                <FilledButton
+                  className="w-full"
+                  onClick={() => removeCard()}
+                  data-testid="cancel-listing-button-id"
+                >
+                  Remove card
+                </FilledButton>
+
+                <OutlineButton
+                  onClick={() => setConfirmStatus(false)}
+                  data-testid="cancel-button-id"
+                >
+                  Cancel
+                </OutlineButton>
+              </div>
             </div>
           </div>
-        </SidePanel>
-      ) : null}
+        </div>
+      </aside>
+    </>
     </div>
   );
 };
