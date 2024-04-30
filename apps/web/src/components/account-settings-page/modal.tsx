f20a2ca4e13f1b85b3b742d1138971c4857c7fc0
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-04-30 20:00:12 +0530

    ui make over for finix widget

--- apps/web/src/components/account-settings-page/modal.tsx
+++ apps/web/src/components/account-settings-page/modal.tsx
@@ -1,27 +1,43 @@
 // Modal.js
 
 import React from "react";
+import { OutlineButton } from "../buttons/outline-button";
 import FinixForm from "./FinixWidget";
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      {/* <div className="absolute inset-0 bg-black opacity-0"></div> */}
-      <div className="bg-white p-6 rounded-md shadow-lg">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-lg font-semibold">Add bank details</h2>
-          <button
-            className="text-gray-500 hover:text-gray-700"
-            onClick={onClose}
-          >
-            Close
-          </button>
+    <>
+      {isOpen && (
+        <div
+          className={`fixed left-0 top-0 z-20 h-[100dvh] w-screen backdrop-blur `}
+        >
+          <div className="h-screen bg-[#00000099]" />
         </div>
-        <FinixForm onClose={onClose} />
-      </div>
-    </div>
+      )}
+      <aside
+        className={`!duration-400 fixed right-0 top-1/2 z-20 flex h-[90dvh] w-[80vw] -translate-y-1/2 flex-col overflow-y-hidden border border-stroke bg-white shadow-lg transition-all ease-linear sm:w-[500px] md:h-[100dvh] ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="bg-white p-6 rounded-md h-full">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-lg font-semibold">Add bank details</h2>
+            <button
+              className="text-gray-500 hover:text-gray-700"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
+          <FinixForm onClose={onClose} />
+          <OutlineButton onClick={onClose} className="w-full">
+            Cancel
+          </OutlineButton>
+        </div>
+      </aside>
+    </>
   );
 };
 
