8cf092861c6c7dabefd45dffbac36f3d016e99ad
Nara Sankar <nara@golfdistrict.com>
2024-05-03 21:02:25 -0700

    Added more logging to troubleshoot secondary market transactions.

--- apps/web/src/app/api/hyperswitch/route.ts
+++ apps/web/src/app/api/hyperswitch/route.ts
@@ -4,6 +4,7 @@ import { NextResponse, type NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   console.log("hyperswitch webhook called");
   console.log(req);
+  console.log(req.json());
 
   await processHyperSwitchWebhook(await req.json());
   return NextResponse.json({ ok: true });
