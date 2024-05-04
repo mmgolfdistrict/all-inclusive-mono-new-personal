cd04b5c1ce6c921490f51fdcc73ce22c040eac60
Nara Sankar <nara@golfdistrict.com>
2024-05-04 12:21:38 -0700

    Reverted the changes and included the HyperSwitchEvent.

--- packages/services/src/webhooks/hyperswitch.webhook.service.ts
+++ packages/services/src/webhooks/hyperswitch.webhook.service.ts
@@ -99,7 +99,7 @@ export class HyperSwitchWebhookService {
    * hyperSwitchWebhookService.processWebhook(webhook);
    * ```
    */
-  processWebhook = async (req: /* HyperSwitchEvent */ any) => {
+  processWebhook = async (req: HyperSwitchEvent) => {
     console.log("processWebhook outside setTimeout");
     console.log(req);
 
@@ -110,10 +110,16 @@ export class HyperSwitchWebhookService {
     this.logger.info(`Processing webhook: ${req.event_id}`);
     this.logger.info(JSON.stringify(req));
 
-    const eventType: string | undefined = req.status; // req.event_type;
-    const paymentId: string | undefined = req.payment_id; // req.content.object.payment_id;
-    const amountReceived: number | undefined = req.amount_received; // req.content.object.amount_received;
-    const customer_id: string | undefined = req.customer_id; // req.content.object.customer_id;
+    const eventType = req.event_type; //req.status; // req.event_type;
+    const paymentId = req.content.object.payment_id; // req.payment_id; // req.content.object.payment_id;
+    const amountReceived = req.content.object.amount_received; // req.amount_received; // req.content.object.amount_received;
+    const customer_id = req.content.object.customer_id; //req.customer_id; // req.content.object.customer_id;
+
+    console.log(`eventType: ${eventType}`);
+    console.log(`paymentId: ${paymentId}`);
+    console.log(`amountReceived: ${amountReceived}`);
+    console.log(`customer_id: ${customer_id}`);
+
     if (!customer_id) throw new Error("Customer id not found");
     if (!paymentId) throw new Error("Payment id not found");
     if (!amountReceived) throw new Error("Amount received not found");
@@ -124,10 +130,10 @@ export class HyperSwitchWebhookService {
 
     switch (eventType) {
       case "payment_succeeded":
-      case "succeeded":
+        // case "succeeded":
         return this.paymentSuccessHandler(customerCart, amountReceived, paymentId, customer_id);
       case "payment_failed":
-      case "failed":
+        // case "failed":
         return this.paymentFailureHandler(customer_id);
       default:
         this.logger.warn(`Unhandled event type: ${eventType}`);
