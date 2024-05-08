9ffda0017d2d94f5534b042da89f9d049f78ccde
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-05-08 20:40:11 +0530

    added profenity check

---
+++ packages/api/src/routers/profanity.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const profanityRouter = createTRPCRouter({
+    checkProfanity: publicProcedure
+        .input(
+            z.object({
+                text: z.string(),
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            return ctx.serviceFactory.getProfanityService().isProfane(input.text);
+        }),
+});
