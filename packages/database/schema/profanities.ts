43b151d56b5b1c9285ea17c7501e36eb74fa999a
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-05-08 21:34:07 +0530

    primary key added

--- packages/database/schema/profanities.ts
+++ packages/database/schema/profanities.ts
@@ -5,7 +5,7 @@ import { sql } from "drizzle-orm";
 export const profanities = mySqlTable(
     "profanity",
     {
-        id: varchar("id", { length: 36 }).notNull(),
+        id: varchar("id", { length: 36 }).notNull().primaryKey().unique(),
         profanityText: varchar("profanityText", { length: 50 }).notNull(),
         createdDateTime: datetime("createdDateTime", { mode: "string", fsp: 3 })
             .default(sql`CURRENT_TIMESTAMP(3)`)
