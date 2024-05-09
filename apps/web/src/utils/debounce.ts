b1a484069d2331f5685be71b84f4870dce393403
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-05-09 12:11:33 +0530

    fix onsubmit check & debounce profanitycheck call

---
+++ apps/web/src/utils/debounce.ts
@@ -0,0 +1,9 @@
+export const debounceFunction = (func, delay: number) => {
+    let timeoutId;
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
+};
