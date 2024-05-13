c8b3c5cf2f1f1142516cfcfeb8af61178c6b797c
Manish <manish.pandey2697@gmail.com>
2024-05-14 02:04:04 +0530

    erroraudit initial changes

--- packages/services/src/webhooks/logging.service.ts
+++ packages/services/src/webhooks/logging.service.ts
@@ -13,6 +13,20 @@ interface AuditLog {
   lastUpdatedDateTime?: Date;
 }
 
+interface ErrorLog {
+  id: string;
+  applicationName: string;
+  clientIP: string;
+  userId: string;
+  url: string;
+  userAgent: string;
+  message: string;
+  stackTrace: string;
+  additionalDetailsJSON: string;
+  createdDateTime: string;
+  lastUpdatedDateTime: string;
+}
+
 export class LoggerService {
   auditLog = async (data: AuditLog) => {
     try {
@@ -35,4 +49,25 @@ export class LoggerService {
       return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
     }
   };
+  errorLog = async (data: ErrorLog) =>{
+    try {
+      const res = await fetch(`${process.env.QSTASH_BASE_URL}${process.env.QSTASH_AUDIT_TOPIC}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+          "Upstash-Delay": `${process.env.QSTASH_AUDIT_DELAY_IN_SECONDS}s`,
+        },
+      });
+      if (res.ok) {
+        return NextResponse.json({ message: "Message successfully sent." });
+      } else {
+        return NextResponse.json({ error: "Oops! Something is wrong." }, { status: 500 });
+      }
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
+    }
+  }
 }
