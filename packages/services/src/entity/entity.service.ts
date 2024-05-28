7f9de76acfc4d69404d783de9e1bfd900bd0aa9c
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-05-28 14:21:10 +0530

    undo entity changes

--- packages/services/src/entity/entity.service.ts
+++ packages/services/src/entity/entity.service.ts
@@ -87,8 +87,7 @@ export class EntityService {
    * @TODO entity return as url
    */
   getEntityFromDomain = async (domain: string, rootDomain: string) => {
-    //const subdomain = domain.endsWith(`.${rootDomain}`) ? domain.replace(`.${rootDomain}`, "") : null;
-    const subdomain = "localhost-2";
+    const subdomain = domain.endsWith(`.${rootDomain}`) ? domain.replace(`.${rootDomain}`, "") : null;
     this.logger.debug(`getEntityFromDomain called for: ${subdomain ? subdomain : domain}`);
 
     const query = this.database
