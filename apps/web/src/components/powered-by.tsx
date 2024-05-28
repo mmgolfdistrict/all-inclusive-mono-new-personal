673872f5587a842e919d0308360297eb2de33fb1
Saloni Jain <verti_saloni_jain@golfdistrict.com>
2024-05-28 17:13:51 +0530

    ENG-229-logo changes

--- apps/web/src/components/powered-by.tsx
+++ apps/web/src/components/powered-by.tsx
@@ -1,5 +1,6 @@
 import { type ComponentProps } from "react";
 import { GolfDistrict } from "./icons/golf-district";
+import { BlurImage } from "./images/blur-image";
 
 export const PoweredBy = (props: ComponentProps<"div">) => {
   return (
@@ -7,13 +8,21 @@ export const PoweredBy = (props: ComponentProps<"div">) => {
       className="flex flex-col items-center justify-center md:flex-row md:gap-1"
       {...props}
     >
-      <div className="text-[10px] text-primary-black md:text-[12px]">
+      <sup className="text-[10px] text-primary-black md:text-[12px]">
         Powered by
-      </div>
-      <GolfDistrict
+      </sup>
+      {/* <GolfDistrict
         id={props?.id ?? "powered-by"}
         color="black"
         className="w-[90px] md:w-[80px]"
+      /> */}
+      <BlurImage
+        src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/logo.png`}
+        // src="https://d3oay9b1er2tcs.cloudfront.net/fri_dec_08_2023-arrowood-_3_.jpg"
+        alt="golf district logo"
+        width={150}
+        height={100}
+        className="w-[50px] object-fit"
       />
     </div>
   );
