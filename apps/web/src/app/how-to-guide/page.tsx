d581968de18f621955e2502c8509fb6ad79c46c4
Nara Sankar <nara@golfdistrict.com>
2024-06-04 15:41:06 -0700

    Adjusting height.

--- apps/web/src/app/how-to-guide/page.tsx
+++ apps/web/src/app/how-to-guide/page.tsx
@@ -6,22 +6,15 @@ export default function HowToGuide() {
       <div className="mx-auto flex items-center justify-between px-4 md:max-w-[1360px] md:px-6">
         <GoBack href="" usePrevRoute={true} text={`Back`} />
       </div>
-      <section
-        className="mx-auto flex w-full flex-col pt-4 md:max-w-[1360px] min-h-full
- max-h-full md:gap-4 md:px-6"
-      >
+      <section className="mx-auto flex w-full flex-col pt-4 md:max-w-[1360px] md:gap-4 md:px-6">
         <h1 className="pb-4 text-center text-2xl text-secondary-black md:pb-0 md:text-3xl">
           How to Guide
         </h1>
-        <section
-          className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px] min-h-full
- max-h-full"
-        >
+        <section className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px]">
           <iframe
             src="https://scribehow.com/page/Golf_District__How_to_Guide__Kg_zsvNST2C3xhJXdbeyog"
             title="Golf District How to Guide"
-            className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px] min-h-full
-            max-h-full"
+            className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px] md:h-full"
           ></iframe>
         </section>
       </section>
