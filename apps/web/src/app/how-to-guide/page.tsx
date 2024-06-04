fb1ad4891ef263e8dd84018a97577095f4b9c96d
Nara Sankar <nara@golfdistrict.com>
2024-06-04 15:12:05 -0700

    Setting height for the how to guide iframe.

--- apps/web/src/app/how-to-guide/page.tsx
+++ apps/web/src/app/how-to-guide/page.tsx
@@ -6,11 +6,11 @@ export default function HowToGuide() {
       <div className="mx-auto flex items-center justify-between px-4 md:max-w-[1360px] md:px-6">
         <GoBack href="" usePrevRoute={true} text={`Back`} />
       </div>
-      <section className="mx-auto flex w-full flex-col pt-4 md:max-w-[1360px] md:gap-4 md:px-6">
+      <section className="mx-auto flex w-full flex-col pt-4 md:max-w-[1360px] max-h-full md:gap-4 md:px-6">
         <h1 className="pb-4 text-center text-2xl text-secondary-black md:pb-0 md:text-3xl">
           How to Guide
         </h1>
-        <section className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px]">
+        <section className="mx-auto flex w-full flex-col gap-4 md:max-w-[1174px] max-h-full">
           <iframe
             src="https://scribehow.com/page/Golf_District__How_to_Guide__Kg_zsvNST2C3xhJXdbeyog"
             title="Golf District How to Guide"
