a330c7fab01cf769398d93605c164ec0bdb15899
Nara Sankar <nara@golfdistrict.com>
2024-06-04 20:51:45 -0700

    Added the justification back.

--- apps/web/src/components/accordion/accordion-item.tsx
+++ apps/web/src/components/accordion/accordion-item.tsx
@@ -20,7 +20,7 @@ export const AccordionItem = ({
           className={`group w-full`}
           data-testid="accordion-button-id"
         >
-          <div className="flex flex-1 cursor-pointer items-left bg-white p-4 text-[16px] text-secondary-black transition-all md:text-[20px] md:group-data-[state=closed]:rounded-xl md:group-data-[state=open]:rounded-t-xl">
+          <div className="flex flex-1 cursor-pointer items-left justify-between bg-white p-4 text-[16px] text-secondary-black transition-all md:text-[20px] md:group-data-[state=closed]:rounded-xl md:group-data-[state=open]:rounded-t-xl">
             <div>{title}</div>
             <DownChevron
               fill="#6D777C"
