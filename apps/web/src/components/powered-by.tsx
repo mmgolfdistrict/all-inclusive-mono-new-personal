15a9820989c09a400c9eff4f0772cfa0ab9c0dfe
Saloni Jain <verti_saloni_jain@golfdistrict.com>
2024-06-13 23:30:40 +0530

    changes in logos

--- apps/web/src/components/powered-by.tsx
+++ apps/web/src/components/powered-by.tsx
@@ -1,6 +1,10 @@
 import { type ComponentProps } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { BlurImage } from "./images/blur-image";
 
 export const PoweredBy = (props: ComponentProps<"div">) => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <div
       className="flex flex-col items-center justify-center md:flex-row md:gap-1"
@@ -9,7 +13,7 @@ export const PoweredBy = (props: ComponentProps<"div">) => {
       <sup className="text-[10px] text-primary-black md:text-[12px]">
         Powered by
       </sup>
-      <img alt="golf district logo" src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/headerlogo.png`}></img>
+      <img alt="golf district logo" src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/${isMobile ? "mobileheaderlogo.png" : "desktopheaderlogo.png"}`} />
     </div>
   );
 };
