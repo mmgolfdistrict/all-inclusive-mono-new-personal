c419b3a95623a2116a7fbda6f074a9051adc72f4
Saloni Jain <verti_saloni_jain@golfdistrict.com>
2024-06-13 21:00:27 +0530

    ENG-248-header and footer logo changes

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -1,24 +1,13 @@
-import { useCourseContext } from "~/contexts/CourseContext";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { GolfDistrict } from "../icons/golf-district";
-import { BlurImage } from "../images/blur-image";
 
 export const Footer = () => {
-  const { course } = useCourseContext();
-  const pathname = usePathname();
   return (
     <div className="relative flex w-full flex-col-reverse gap-2 bg-primary p-6 text-sm text-white md:flex-row md:items-center md:justify-between md:p-8">
       <div>Copyright {new Date().getFullYear()}</div>
       <div className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform items-end gap-1 md:flex">
         <span>Powered by</span>
-        <BlurImage
-          src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/footerlogo.png`}
-          alt="golf district logo"
-          className="w-[50px] object-fit"
-          width={150}
-          height={50}
-        />
+        <img alt="golf district logo" src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/footerlogo.png`} />
       </div>
       <div className="flex flex-col items-start gap-2 md:items-end lg:flex-row lg:items-center">
         <Link href={`/faq`} data-testid="footer-help-id">
