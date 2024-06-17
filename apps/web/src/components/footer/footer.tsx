7dd4e5442b0bb3d63f5d3660a25681e28467e8e7
jignesh <verti_jignesh_vyas@golfdistrict.com>
2024-06-17 12:08:10 +0530

    undo unnecessary changes

--- apps/web/src/components/footer/footer.tsx
+++ apps/web/src/components/footer/footer.tsx
@@ -7,7 +7,10 @@ export const Footer = () => {
       <div>Copyright {new Date().getFullYear()}</div>
       <div className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform gap-1 md:flex items-center justify-center">
         <span className="text-[10px] md:text-[12px]">Powered by</span>
-        {/* <img alt="golf district logo" src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/desktopfooterlogo.png`} /> */}
+        <img
+          alt="golf district logo"
+          src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/desktopfooterlogo.png`}
+        />
       </div>
       <div className="flex flex-col items-start gap-2 md:items-end lg:flex-row lg:items-center">
         <Link href={`/faq`} data-testid="footer-help-id">
--- apps/web/src/components/powered-by.tsx
+++ apps/web/src/components/powered-by.tsx
@@ -13,12 +13,12 @@ export const PoweredBy = (props: ComponentProps<"div">) => {
       <sup className="text-[10px] text-primary-black md:text-[12px]">
         Powered by
       </sup>
-      {/* <img
+      <img
         alt="golf district logo"
         src={`https://${process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL}/${
           isMobile ? "mobileheaderlogo.png" : "desktopheaderlogo.png"
         }`}
-      /> */}
+      />
     </div>
   );
 };
