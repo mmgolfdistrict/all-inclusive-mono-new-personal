da85aea8ec851644c0e68a76b246e210ac7c0af3
Nara Sankar <nara@golfdistrict.com>
2024-07-02 20:22:40 -0700

    Placed tooltip in checkout screen in the same line.

--- apps/web/src/components/checkout-page/order-summary.tsx
+++ apps/web/src/components/checkout-page/order-summary.tsx
@@ -21,11 +21,13 @@ export const OrderSummary = ({
     <section className="mx-auto flex w-full h-fit flex-col gap-4 bg-white px-3 py-2 md:rounded-xl md:p-6 md:py-4">
       <h1 className="text-center md:text-start">Order Summary</h1>
       <div className="rounded-md bg-primary p-2 text-white">
-        <h2 className="text-lg">Plans change. No worries!</h2>
-        <Tooltip
-          trigger={<Info className="h-[14px] w-[14px]" />}
-          content="Resell your tee time from our website. Selling outside from our website is not allowed, and both the buyer and seller may forfeit their money and time."
-        />
+        <h2 className="text-lg">
+          Plans change. No worries!
+          <Tooltip
+            trigger={<Info className="h-[14px] w-[14px]" />}
+            content="Resell your tee time from our website. Selling outside from our website is not allowed, and both the buyer and seller may forfeit their money and time."
+          />
+        </h2>
         {/* <p className="text-sm">
           Resell your tee time from our website. Selling outside from our
           website is not allowed, and both the buyer and seller may forfeit
