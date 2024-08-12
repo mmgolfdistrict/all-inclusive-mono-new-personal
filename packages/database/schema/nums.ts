a57935c0bd398aaa94f959550a7d1a875f641159
Nara Sankar <nara@golfdistrict.com>
2024-08-12 10:19:25 -0700

    Modified the schema definition for booking isTest default value and nums primary key.

--- packages/database/schema/nums.ts
+++ packages/database/schema/nums.ts
@@ -3,7 +3,7 @@ import { int } from "drizzle-orm/mysql-core";
 import { mySqlTable } from "./_table";
 
 export const nums = mySqlTable("nums", {
-  value: int("value").notNull(),
+  value: int("value").notNull().primaryKey(),
 });
 
 export type InsertSystemNotification = InferSelectModel<typeof nums>;
