601dc9691256d511e7f01e207ba465961c797d65
jignesh <verti_jignesh_vyas@golfdistrict.com>
2025-01-03 00:05:40 +0530

    build fix

--- packages/services/src/booking/booking.service.ts
+++ packages/services/src/booking/booking.service.ts
@@ -3599,7 +3599,7 @@ export class BookingService {
           cartFeeTaxPercentTotal,
           additionalTaxes,
         },
-        source
+        source,
         additionalNoteFromUser,
         needRentals
       })
