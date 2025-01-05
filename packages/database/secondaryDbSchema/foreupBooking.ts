4c2d347745c9552e87cd4d2f35318f28f71d0cc3
Nara Sankar <nara@golfdistrict.com>
2025-01-05 12:27:40 -0800

    ENG-827: Extended player count to small int to support for tournaments.

--- packages/database/secondaryDbSchema/foreupBooking.ts
+++ packages/database/secondaryDbSchema/foreupBooking.ts
@@ -1,6 +1,6 @@
 import type { InferSelectModel } from "drizzle-orm";
 import { sql } from "drizzle-orm";
-import { boolean, datetime, tinyint, unique, varchar } from "drizzle-orm/mysql-core";
+import { boolean, datetime, smallint, tinyint, unique, varchar } from "drizzle-orm/mysql-core";
 import { mySqlTable } from "./_table";
 
 export const foreupBooking = mySqlTable(
@@ -16,7 +16,7 @@ export const foreupBooking = mySqlTable(
     attributes_start: varchar("attributes_start", { length: 25 }),
     attributes_end: varchar("attributes_end", { length: 25 }),
     attributes_duration: tinyint("attributes_duration"),
-    attributes_playerCount: tinyint("attributes_playerCount"),
+    attributes_playerCount: smallint("attributes_playerCount"),
     attributes_holes: tinyint("attributes_holes"),
     attributes_noShowCount: tinyint("attributes_noShowCount"),
     attributes_title: varchar("attributes_title", { length: 256 }),
--- packages/database/secondaryDbSchema/foreupBookingSaleItem.ts
+++ packages/database/secondaryDbSchema/foreupBookingSaleItem.ts
@@ -1,6 +1,6 @@
 import type { InferSelectModel } from "drizzle-orm";
 import { sql } from "drizzle-orm";
-import { datetime, decimal, double, tinyint, unique, varchar } from "drizzle-orm/mysql-core";
+import { datetime, decimal, double, smallint, tinyint, unique, varchar } from "drizzle-orm/mysql-core";
 import { mySqlTable } from "./_table";
 
 export const foreupBookingSaleItem = mySqlTable(
@@ -15,7 +15,7 @@ export const foreupBookingSaleItem = mySqlTable(
     attributes_total: double("attributes_total"),
     attributes_subTotal: double("attributes_subTotal"),
     attributes_tax: double("attributes_tax"),
-    attributes_quantity: tinyint("attributes_quantity"),
+    attributes_quantity: smallint("attributes_quantity"),
     attributes_unitPrice: double("attributes_unitPrice"),
     attributes_unitCost: double("attributes_unitCost"),
     attributes_discountPercent: double("attributes_discountPercent"),
@@ -25,7 +25,7 @@ export const foreupBookingSaleItem = mySqlTable(
     attributes_price_category: varchar("attributes_price_category", { length: 36 }),
     attributes_type: varchar("attributes_type", { length: 15 }),
     attributes_holes: tinyint("attributes_holes"),
-    attributes_player_count: tinyint("attributes_player_count"),
+    attributes_player_count: smallint("attributes_player_count"),
     attributes_teesheet_name: varchar("attributes_teesheet_name", { length: 255 }),
     attributes_teetime_id: varchar("attributes_teetime_id", { length: 36 }),
     attributes_department: varchar("attributes_department", { length: 36 }),
