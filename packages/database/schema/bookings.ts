f659ce498b78d1c790e42491adf5bf33b172a9a8
Nara Sankar <nara@golfdistrict.com>
2025-01-19 14:48:53 -0800

    ENG-000: Updated default values for columns.

--- packages/database/schema/bookings.ts
+++ packages/database/schema/bookings.ts
@@ -49,10 +49,10 @@ export const bookings = mySqlTable(
     totalCartFeeTaxAmount: int("totalCartFeeTaxAmount").notNull().default(0),
     totalWeatherGuaranteeTaxAmount: int("totalWeatherGuaranteeTaxAmount").notNull().default(0),
     totalMarkupFeeTaxAmount: int("totalMarkupFeeTaxAmount").notNull().default(0),
-    source: varchar("source", { length: 50 }).default(""),
+    source: varchar("source", { length: 50 }),
     customerComment: varchar("customerComment", { length: 200 }),
     needClubRental: boolean("needClubRental").default(false).notNull(),
-    },
+  },
   (table) => {
     return {
       listIdIdx: index("Booking_listId_idx").on(table.listId),
