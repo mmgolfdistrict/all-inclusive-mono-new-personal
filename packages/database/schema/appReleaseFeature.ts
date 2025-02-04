31d624844d7d82b029868b25fbf99f12f2a10f3e
Govinda Mandal <verti_govinda_mandal@golfdistrict.com>
2025-02-04 21:11:11 +0530

    ENG-859 updated name length to 255 and description length to 512 of the table appReleaseFeature

--- packages/database/schema/appReleaseFeature.ts
+++ packages/database/schema/appReleaseFeature.ts
@@ -7,8 +7,8 @@ export const appReleaseFeature = mySqlTable("appReleaseFeature", {
   id: varchar("id", { length: 36 }).notNull().primaryKey().unique(),
   releaseId: varchar("releaseId", { length: 36 }).notNull(),
   ticketNumber: varchar("ticketNumber", { length: 15 }),
-  name: varchar("name", { length: 25 }).notNull(),
-  description: varchar("description", { length: 25 }).notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 512 }).notNull(),
   engineerName: varchar("engineerName", { length: 50 }),
   isVisible: tinyint("isVisible").default(1),
   createdDateTime: datetime("createdDateTime", { mode: "string", fsp: 3 })
