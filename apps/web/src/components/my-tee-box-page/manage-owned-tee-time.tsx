89aadf15e9fb3e389fdd49fce7851ff4f22d90d4
Saloni Thakur <salonithakur@MacBook-Pro.local>
2025-03-13 20:09:36 +0530

    ENG-969: Invite Players Does Not Work

--- apps/web/src/components/my-tee-box-page/manage-owned-tee-time.tsx
+++ apps/web/src/components/my-tee-box-page/manage-owned-tee-time.tsx
@@ -421,8 +421,7 @@ export const ManageOwnedTeeTime = ({
                             {!friend.currentlyEditing ? (
                               <div className="mx-auto w-full rounded-lg bg-secondary-white px-4 py-1 flex justify-between text-[16px] font-semibold outline-none">
                                 <div>{index === 0 ? "You" : friend.name}</div>
-                                {index !== 0 &&
-                                course?.supportsPlayerNameChange ? (
+                                {index !== 0 ? (
                                   <button
                                     onClick={() => removeFriend(friend.slotId)}
                                   >
