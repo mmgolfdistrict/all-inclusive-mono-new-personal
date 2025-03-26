321df875f1111f0d0241cae551d5ab31d74083d5
Nara Sankar <145275696+nara-golfdistrict-com@users.noreply.github.com>
2025-03-26 07:56:49 -0700

    Revert "ENG-969: Invite Players Does Not Work"

--- apps/web/src/components/my-tee-box-page/manage-owned-tee-time.tsx
+++ apps/web/src/components/my-tee-box-page/manage-owned-tee-time.tsx
@@ -421,14 +421,7 @@ export const ManageOwnedTeeTime = ({
                           >
                             {!friend.currentlyEditing ? (
                               <div className="mx-auto w-full rounded-lg bg-secondary-white px-4 py-1 flex justify-between text-[16px] font-semibold outline-none">
-                                <div
-                                  style={{
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                  }}
-                                >
-                                  {index === 0 ? "You" : friend.name}
-                                </div>
+                                <div>{index === 0 ? "You" : friend.name}</div>
                                 {index !== 0 &&
                                 course?.supportsPlayerNameChange ? (
                                   <button
