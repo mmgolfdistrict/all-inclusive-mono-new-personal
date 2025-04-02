33174aa4c662a3b8c32818f6b88d3d1e45973ce8
Saloni Thakur <salonithakur@MacBook-Pro.local>
2025-04-03 00:41:12 +0530

    ENG-957: Missing Back Button in Help, How to Guide, T&C and Privacy Policy

--- apps/web/src/app/terms-of-service/page.tsx
+++ apps/web/src/app/terms-of-service/page.tsx
@@ -4,17 +4,38 @@ import { GoBack } from "~/components/buttons/go-back";
 import { api } from "~/utils/api";
 import { LoadingContainer } from "../[course]/loader";
 
-export default function TermsOfService() {
+export default function TermsOfService({
+  params,
+}: {
+  params: { course: string };
+}) {
   const { data: termsOfServices, isLoading } =
     api.user.getS3HtmlContent.useQuery({ keyName: "termsofservice.html" });
+
+  const courseId = params.course;
+
+  const { data: systemNotifications } =
+    api.systemNotification.getSystemNotification.useQuery({});
+
+  const { data: courseGlobalNotification } =
+    api.systemNotification.getCourseGlobalNotification.useQuery({
+      courseId: courseId ?? "",
+    });
+
+  const notificationsCount =
+    (systemNotifications ? systemNotifications.length : 0) +
+    (courseGlobalNotification ? courseGlobalNotification.length : 0);
+
+  const marginTop =
+    notificationsCount > 0 ? `${(notificationsCount * 30) + 125}px` : "0";
 
   return (
     <>
       <LoadingContainer isLoading={isLoading}>
         <div></div>
       </LoadingContainer>
-      <div className="flex items-center justify-between px-4 md:px-6 mt-8">
-        <GoBack href={`/`} text={`Back`} />
+      <div style={{ marginTop }} className="mx-auto flex items-center justify-between px-4 md:max-w-[1360px] md:px-6">
+        <GoBack href="" usePrevRoute={true} text={`Back`} />
       </div>
       {termsOfServices && (
         <main className="bg-secondary-white py-4 md:py-6 ">
