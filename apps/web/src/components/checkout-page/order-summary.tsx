ee9752e4aff4622929a6e89c71d758ece6215190
Saloni Thakur <salonithakur@MacBook-Pro.local>
2025-05-20 22:47:05 +0530

    ENG-1166: Cease and Desist Verbiage Change

--- apps/web/src/components/checkout-page/order-summary.tsx
+++ apps/web/src/components/checkout-page/order-summary.tsx
@@ -27,12 +27,13 @@ export const OrderSummary = ({
   return (
     <section className="mx-auto flex w-full h-fit flex-col gap-4 bg-white px-3 py-2 md:rounded-xl md:p-6 md:py-4">
       <h1 className="text-center md:text-start">Order Summary</h1>
-      <div className="rounded-md bg-primary p-2 text-white">
+      <div className="rounded-md bg-gray-300 p-2 text-black">
         <h2 className="text-lg">
           Plans change. No worries! Resell your time.{" "}
           <Tooltip
-            trigger={<Info className="h-[14px] w-[14px]" />}
-            content="Easily resell your tee time through our website. Please note that selling outside our platform is prohibited and may result in both the buyer and seller forfeiting their money and time."
+            className="!text-black"
+            trigger={<Info color="#000000" className="h-[14px] w-[14px]" />}
+            content="Easily resell your tee time through our Golf District platforms. Tee times bought and resold through Golf District—an approved technology partner—are verified and supported because they sync directly with the golf course’s official tee sheet. Reselling through unaffiliated platforms may not be compatible with course systems and could result in an invalid booking."
           />
         </h2>
       </div>
