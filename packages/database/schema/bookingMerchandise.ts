02ca81114aff0a64a504c932509c34ab9448b47e
jignesh <verti_jignesh_vyas@golfdistrict.com>
2025-06-11 16:41:40 +0530

    update: column names on bookingMarchandise

--- packages/database/schema/bookingMerchandise.ts
+++ packages/database/schema/bookingMerchandise.ts
@@ -14,8 +14,8 @@ export const bookingMerchandise = mySqlTable('bookingMerchandise', {
   lastUpdatedDateTime: datetime('lastUpdatedDateTime', { mode: 'string', fsp: 3 })
     .default(sql`CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)`)
     .notNull(),
-  merchandiseAmount: int("merchandiseAmount").default(0),
-  merchandiseTaxAmount: int("merchandiseTaxAmount").default(0),
+  merchandiseAmountPerItem: int("merchandiseAmountPerItem").default(0),
+  totalMerchandiseItemTaxAmount: int("totalMerchandiseItemTaxAmount").default(0),
 },
   (table) => ({
     bookingIdIdx: index("bookingMerchandise_bookingId_idx").on(table.bookingId),
