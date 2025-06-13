eda16d90ee9964b6535c4e4b59cce08a56dae152
Manav GD <ManavGD@apples-MacBook-Pro.local>
2025-06-13 20:57:40 +0530

    ENG-1148:Sync tempUser column with internal web service

--- packages/database/secondaryDbSchema/tempUser.ts
+++ packages/database/secondaryDbSchema/tempUser.ts
@@ -1,12 +1,13 @@
+
+import type { InferSelectModel } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import { datetime, tinyint, varchar } from "drizzle-orm/mysql-core";
 import { mySqlTable } from "./_table";
-import { InferSelectModel, sql } from "drizzle-orm";
-
 
 export const tempUser = mySqlTable("tempUser", {
     id: varchar("id", { length: 36 }).notNull().primaryKey(),
     name: varchar("name", { length: 255 }),
-    email: varchar("name", { length: 255 }),
+    email: varchar("email", { length: 255 }),
     phoneNumberCountryCode: tinyint("phoneNumberCountryCode"),
     phoneNumber: varchar("phoneNumber", { length: 25 }),
     createdDateTime: datetime("createdDateTime", { mode: "string", fsp: 3 }).notNull().default(sql`CURRENT_TIMESTAMP(3)`),
