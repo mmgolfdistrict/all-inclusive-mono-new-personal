8ccdf92ef7445d4e49c90222cadf4bb4783cd5bf
Manav GD <ManavGD@192.168.1.13>
2025-06-19 20:38:25 +0530

    ENG-1148:Sync with secondary database phone log table

--- packages/database/secondaryDbSchema/phoneCallLogs.ts
+++ packages/database/secondaryDbSchema/phoneCallLogs.ts
@@ -1,7 +1,7 @@
-import type { InferInsertModel} from "drizzle-orm";
+import type { InferInsertModel } from "drizzle-orm";
 import { relations, sql } from "drizzle-orm";
-import {  datetime, varchar } from "drizzle-orm/mysql-core";
-import { mySqlTable } from "../schema/_table";
+import { datetime, text, varchar } from "drizzle-orm/mysql-core";
+import { mySqlTable } from "./_table";
 
 export const phoneCallLogs = mySqlTable(
     "phoneCallLog",
@@ -10,6 +10,9 @@ export const phoneCallLogs = mySqlTable(
         forwardingPhoneNumber: varchar("forwardingPhoneNumber", { length: 25 }).notNull(),
         callerPhoneNumber: varchar("callerPhoneNumber", { length: 25 }).notNull(),
         sessionId: varchar("sessionId", { length: 36 }).notNull(),
+        callSid: varchar("twillioCallSid", { length: 100 }),
+        recordingSid: varchar("twillioRecordingSid", { length: 100 }),
+        recordingUrl: text("twillioRecordingUrl"),
         createdDateTime: datetime("createdDateTime", { mode: "string", fsp: 3 })
             .default(sql`CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)`)
             .notNull(),
