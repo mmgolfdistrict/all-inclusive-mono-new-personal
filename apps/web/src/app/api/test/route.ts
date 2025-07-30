ccb7448ebea5e3d095c5f9d1e0152168341525d2
Govinda Mandal <verti_govinda_mandal@golfdistrict.com>
2025-07-30 19:48:17 +0530

    ENG-717: Added a test route to check the stack trace with error

--- apps/web/src/app/api/test/route.ts
+++ apps/web/src/app/api/test/route.ts
@@ -2,8 +2,13 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  try {
     const users = await fetch('https://jsonplaceholder.typicode.com/users121');
     const data = await users.json();
-    
-  return NextResponse.json({ message: 'hello worlds', data });
+    throw new Error('Simulated error'); // Simulating an error for testing
+    return NextResponse.json({ message: 'hello worlds', data });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return NextResponse.json({ error: 'Failed to fetch users' }, { status: 500 });
+  } 
 }
