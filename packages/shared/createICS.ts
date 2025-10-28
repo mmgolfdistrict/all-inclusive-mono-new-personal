dbd63a4663dc18c16b867d239214187db5f6ec80
Saloni Thakur <salonithakur@Saloni-Thakur-MacBook-Pro.local>
2025-10-29 00:18:21 +0530

    ENG-1196: Remove Code for .ics Attachment

--- packages/shared/createICS.ts
+++ packages/shared/createICS.ts
@@ -2,6 +2,7 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import { randomUUID } from "crypto";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -76,7 +77,7 @@ export default function createICS(event: Event, sequence = 0): string {
   const formatICSDate = (d: dayjs.Dayjs) => d.format("YYYYMMDDTHHmmss");
 
   const uidBase = event.reservationGroupId ?? event.reservationId ?? "reservation";
-  const uid = `${uidBase}-${Date.now()}-${Math.random().toString(36).substring(2, 10)}@golfdistrict.com`;
+  const uid = `${uidBase}-${Date.now()}-${randomUUID()}@golfdistrict.com`;
 
   const dtStamp = dayjs.utc().format("YYYYMMDDTHHmmss") + "Z";
   const dtStart = formatICSDate(startMoment);
