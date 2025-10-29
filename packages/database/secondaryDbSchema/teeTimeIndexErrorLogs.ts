8631e370fff890fda102df56e5b12fe368d88cb1
jignesh <verti_jignesh_vyas@golfdistrict.com>
2025-10-29 21:23:57 +0530

    feat: add index for fromDay in teeTimeIndexErrorLog table

--- packages/database/secondaryDbSchema/teeTimeIndexErrorLogs.ts
+++ packages/database/secondaryDbSchema/teeTimeIndexErrorLogs.ts
@@ -22,6 +22,7 @@ export const teeTimeIndexErrorLog = mySqlTable(
   (table) => ({
     courseIdIdx: index("TeeTimeIndexErrorLog_courseId_idx").on(table.courseId),
     providerIdIdx: index("TeeTimeIndexErrorLog_providerId_idx").on(table.providerId),
+    fromDayIdx: index("TeeTimeIndexErrorLog_fromDay_idx").on(table.fromDay),
   })
 );
 
