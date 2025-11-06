1944af14764b8b062c0ead61b0d7c7175e3cc060
mmgolfdistrict <verti_manav_mandal@golfdistrict.com>
2025-11-06 20:40:06 +0530

    Added a new column extrenalRefundId and added a new type AI GENERATED in status column.

--- packages/database/schema/bookingRefund.ts
+++ packages/database/schema/bookingRefund.ts
@@ -8,7 +8,8 @@ export const bookingRefund = mySqlTable("bookingRefund",
         id: varchar("id", { length: 191 }).notNull().primaryKey(),
         bookingId: varchar('bookingId', { length: 36 }).notNull(),
         refundAmount: int("refundAmount").notNull(),
-        status: varchar("status", { length: 15 }).notNull().$type<"PENDING" | "INITIATED" | "COMPLETED" | "REJECTED">(),
+        status: varchar("status", { length: 15 }).notNull().$type<"PENDING" | "INITIATED" | "COMPLETED" | "REJECTED" | "AI GENERATED">(),
+        extrenalRefundId: varchar("extrenalRefundId", { length: 100 }),
         createdDateTime: datetime("createdDateTime", { mode: "string", fsp: 3 })
             .notNull()
             .default(sql`CURRENT_TIMESTAMP(3)`),
