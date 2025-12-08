48d4cb4c48738d7c0ed494aefa13328f6226ac61
Govinda Mandal <verti_govinda_mandal@golfdistrict.com>
2025-12-08 19:09:00 +0530

    ENG-1437: Added 5 seconds timeout in vitest config

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -1,7 +1,9 @@
-import '@testing-library/jest-dom'
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-
+import '@testing-library/jest-dom';
+import { cleanup, configure } from '@testing-library/react';
+import { afterEach } from 'vitest';
+// Increase the async timeout to 5000ms for diagnosis
+configure({ asyncUtilTimeout: 5000 });
+// automatically unmount and cleanup DOM after the test is finished.
 afterEach(() => {
-    cleanup()
-})
+    cleanup();
+});
